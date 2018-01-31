ab6a792ca834d78d44b64212b81f74d240a5bfe2
이재범 <btv9200@gmail.com>
2018-01-31 16:28:47 +0900

    code refactoring error in search, fix search bug

--- app/src/main/java/com/cm/secret_recipe/SearchResult/searchResultViewHolder.java
+++ app/src/main/java/com/cm/secret_recipe/SearchResult/searchResultViewHolder.java
@@ -27,24 +27,25 @@ public class searchResultViewHolder extends RecyclerView.ViewHolder implements V
     @Override
     public void onClick(View v) {
         Intent intent = new Intent(context, ItemOriginalActivity.class);
-        intent.putExtra("typeID", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].typeID);
-        intent.putExtra("menuName", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].menuName);
-        intent.putExtra("menuBrand", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].brandID);
-        intent.putExtra("menuPrice1", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].menuPrice1);
-        intent.putExtra("menuPrice2", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].menuPrice2);
-        intent.putExtra("menuImageResorce", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].drawableID);
-        if (MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].typeID == 1) {
-            intent.putExtra("menuIngredients1", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].ingredients[0]);
-            intent.putExtra("menuIngredients2", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].ingredients[1]);
-            intent.putExtra("menuIngredients3", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].ingredients[2]);
-            intent.putExtra("menuExtras1Name", String.valueOf(MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].extras[0].name));
-            intent.putExtra("menuExtras2Name", String.valueOf(MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].extras[1].name));
-            intent.putExtra("menuExtras3Name", String.valueOf(MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].extras[2].name));
-
-            intent.putExtra("menuExtras1Price", Integer.parseInt(String.valueOf(MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].extras[0].price)));
-            intent.putExtra("menuExtras2Price", Integer.parseInt(String.valueOf(MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].extras[1].price)));
-            intent.putExtra("menuExtras3Price", Integer.parseInt(String.valueOf(MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].extras[2].price)));
-        }
+        intent.putExtra("menu", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()]);
+//        intent.putExtra("typeID", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].typeID);
+//        intent.putExtra("menuName", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].menuName);
+//        intent.putExtra("menuBrand", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].brandID);
+//        intent.putExtra("menuPrice1", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].menuPrice1);
+//        intent.putExtra("menuPrice2", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].menuPrice2);
+//        intent.putExtra("menuImageResorce", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].drawableID);
+//        if (MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].typeID == 1) {
+//            intent.putExtra("menuIngredients1", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].ingredients[0]);
+//            intent.putExtra("menuIngredients2", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].ingredients[1]);
+//            intent.putExtra("menuIngredients3", MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].ingredients[2]);
+//            intent.putExtra("menuExtras1Name", String.valueOf(MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].extras[0].name));
+//            intent.putExtra("menuExtras2Name", String.valueOf(MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].extras[1].name));
+//            intent.putExtra("menuExtras3Name", String.valueOf(MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].extras[2].name));
+//
+//            intent.putExtra("menuExtras1Price", Integer.parseInt(String.valueOf(MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].extras[0].price)));
+//            intent.putExtra("menuExtras2Price", Integer.parseInt(String.valueOf(MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].extras[1].price)));
+//            intent.putExtra("menuExtras3Price", Integer.parseInt(String.valueOf(MenuOriginal.SearchWithName(searchResultActivity.searchName)[getLayoutPosition()].extras[2].price)));
+//        }
         context.startActivity(intent);
     }
 }
